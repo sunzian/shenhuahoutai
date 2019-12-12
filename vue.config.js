a47c1f86a2ab3f13972abdb40cadc2e96496ea11
caaaaaainiao <1443119959@qq.com>
2019-12-12 15:35:14 +0800

    none

--- vue.config.js
+++ vue.config.js
@@ -6,11 +6,11 @@ module.exports = {
         proxy: {
             '/api':{
                 // target: 'http://test.legendpicture.com',
-                // target: 'https://wonder.legendpicture.com',
+                target: 'https://wonder.legendpicture.com',
                 // target: 'https://192.168.43.169:8080',
                 // target: 'https://192.168.31.121:8080',
                 // target: 'https://192.168.10.110:8080',
-                target: 'https://192.168.31.214:8080',
+                // target: 'https://192.168.31.214:8080',
                 changeOrigin:true,
                 pathRewrite:{
                     '/api':''
