7ce3241454b16d96fe1337628eb6f5122da3e79b
caaaaaainiao <1443119959@qq.com>
2019-12-17 13:45:24 +0800

    none

--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,8 @@ module.exports = {
                 // target: 'http://test.legendpicture.com',
                 // target: 'https://wonder.legendpicture.com',
                 // target: 'https://192.168.43.169:8080',
-                // target: 'https://192.168.31.121:8080',
-                target: 'https://192.168.31.219:8080',
+                target: 'https://192.168.31.121:8080',
+                // target: 'https://192.168.31.219:8080',
                 // target: 'https://192.168.31.214:8080',
                 changeOrigin:true,
                 pathRewrite:{
