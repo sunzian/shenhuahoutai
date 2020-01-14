0b4121a60388ce8ab69cde5b01b41952f795d6e9
caaaaaainiao <1443119959@qq.com>
2020-01-14 09:12:18 +0800

    none

--- vue.config.js
+++ vue.config.js
@@ -6,11 +6,11 @@ module.exports = {
         proxy: {
             '/api':{
                 // target: 'http://test.legendpicture.com',
-                // target: 'https://wonder.legendpicture.com',
+                target: 'https://wonder.legendpicture.com',
                 // target: 'https://192.168.43.169:8080',
                 // target: 'https://192.168.43.34:8080',
                 // target: 'https://192.168.1.122:8080',  // gw
-                target: 'https://192.168.31.222:8080',  // zy jzkj
+                // target: 'https://192.168.31.222:8080',  // zy jzkj
                 // target: 'https://192.168.101.10:8080',  // zy heloo2
                 // target: 'https://192.168.31.124:8080',  // sk 5g
                 changeOrigin:true,
